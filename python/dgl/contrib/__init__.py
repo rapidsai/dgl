ade96383aa01f0e5bb50bfb9977e7347f3e5bd1e
Vibhu Jawa <vibhujawa@gmail.com>
2022-06-30 16:11:50 -0700

    Addressed Reviews for CuGraphStorage

--- python/dgl/contrib/__init__.py
+++ python/dgl/contrib/__init__.py
@@ -1,6 +1,10 @@
 from . import sampling
 from . import graph_store
-from . import cugraph
 from .dis_kvstore import KVClient, KVServer
 from .dis_kvstore import read_ip_config
 from .unified_tensor import UnifiedTensor
+
+try:
+    from . import cugraph
+except ModuleNotFoundError:
+    pass
